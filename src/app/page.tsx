06d5293a640de38a6b67464dac2fcc5e8aa6e6af
DAMatkov <mzmf098@gmail.com>
2025-11-05 15:57:42 +0300

    Add search icon, post top border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,14 @@ export default function HomePage() {
             <Navbar></Navbar>
 
 
-            <div className="w-full border-gray-100 flex flex-col  justify-center px-10 gap-2 h-[100px]">
-                <Input placeholder="Поиск" className="rounded-3xl h-[35px]"></Input>
-                <div className="w-full flex flex-row items-center gap-2">
+            <div className="w-full border-gray-100 flex flex-col justify-between px-20 gap-2 h-[100px] py-2">
+
+                <div className = "h-[50px] relative">
+
+                    <Input placeholder="Поиск" className="rounded-[18px] h-full border-gray-500"></Input>
+                    <Search className="size-[18px] text-gray-500 absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-3"></Search>
+                </div>
+                <div className="w-full flex flex-row items-center gap-2 h-[35px]">
 
                     <Button variant="outline" className="rounded-3xl bg-blue-500 text-white border-blue-500
                     hover:text-white hover:border-blue-600 hover:bg-blue-600
@@ -33,14 +38,12 @@ export default function HomePage() {
                 </div>
             </div>
 
-            <ScrollArea className="px-10 pt-[1px] h-[calc(100vh-170px)]">
+            <ScrollArea className="px-20 pt-[1px] h-[calc(100vh-170px)]">
                 <div className="grid grid-cols-3 gap-2">
-                    {/*{Array.from({ length: 50 }).map((_, i) => (*/}
-                    {/*    <div key={i} className="p-4 bg-white border-1 h-[200px] rounded-3xl">*/}
-                    {/*        Карточка секции {i + 1}*/}
-                    {/*    </div>*/}
-                    {/*))}*/}
-
+                    <Post/>
+                    <Post/>
+                    <Post/>
+                    <Post/>
                     <Post/>
                     <Post/>
                     <Post/>
@@ -64,19 +67,24 @@ export default function HomePage() {
 function Post(){
     return (
 
-        <div className="flex flex-col p-2 bg-white border-1 rounded-3xl gap-2">
-            <h2 className="text-gray-500">20 ноября 2025 18:57</h2>
-            <h1 className="text-xl">История футбола в МАИ</h1>
-            <p className="text-gray-500">
-                История нашего спортивного клуба началась в 1970 году. Тогда мы набрали ребят и стали их тренировать...
-            </p>
+        <div className="flex flex-col bg-white border-1 border-gray-300 rounded-2xl overflow-hidden">
+            <div className="w-full bg-blue-500 h-3"></div>
 
-            <div className="flex flex-row items-center justify-left gap-3">
+            <div className="flex flex-col gap-3 p-2  py-5">
+                <h2 className="text-gray-500">20 ноября 2025 18:57</h2>
+                <h1 className="text-xl">История футбола в МАИ</h1>
+                <p className="text-gray-500">
+                    История нашего спортивного клуба началась в 1970 году. Тогда мы набрали ребят и стали их тренировать...
+                </p>
 
-                <CircleUserRound className="size-[5vh] text-blue-500" />
+                <div className="flex flex-row items-center justify-left gap-3">
 
-                <span>Ерофеев Иван</span>
+                    <CircleUserRound className="size-[5vh] text-blue-500" />
+
+                    <span>Ерофеев Иван</span>
+                </div>
             </div>
+
         </div>
     );
 }
